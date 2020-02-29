239b4f36b305519412a571f36ee74110538bfec5
Edo <edogeda@yahoo.com>
2020-02-29 11:19:30 -0500

    add Login.java

---
+++ src/test/Logout.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Logout {
+    //create a class: Logout
+    //add it to staging area
+    //comit
+    public static void main(String[] args) {
+        System.out.println("It is time to go home");
+
+    }
+}
